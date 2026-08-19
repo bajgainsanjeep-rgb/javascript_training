3065995eeb0afae1f2cbe133a08539987d10a593
Sanjeep Bajgain <bajgainsanjeep@gmail.com>
2026-08-19 18:17:45 +0545

    some operators

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let a = 5;
 let b = 10;
 console.log("a+b=",a + b);
+console.log("a-b=",a - b);
+console.log("a*b=",a * b);
+console.log("a/b=",a / b);
+console.log("a%b=",a % b);
+console.log("a**b=",a ** b);
+a++;
+console.log("a++ =",a);
+
+
+if(b > 5){
+    console.log("red ");
+}
+else{
+    console.log("blue");
+
+}
+
+
+
+
+
